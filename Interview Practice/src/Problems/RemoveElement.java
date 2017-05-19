5e09c99c86a8e8427c2834d2657891672faefd81
lij <lij@us.ibm.com>
2017-05-19 14:39:14 -0700

    strStr

---
+++ Interview Practice/src/Problems/RemoveElement.java
@@ -0,0 +1,19 @@
+package Problems;
+
+public class RemoveElement {
+	public static int removeElement(int[] nums, int val) {
+		int x = 0;
+		for(int i = 0; i<nums.length; i++) {
+			if (nums[i] != val) {
+				nums[x]=nums[i];
+				x++;
+			}
+		}
+		return x;
+	}
+
+	public static void main(String[] args) {
+		int[] nums = new int[]{2,3,2,3};
+		System.out.println(removeElement(nums, 3));
+	}
+}
